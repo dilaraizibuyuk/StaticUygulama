6a3755fc00cf37da28591a67332a650852806483
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-11 17:53:39 +0300

    Proje dosyası ekle.

---
+++ StaticUygulama/Hesap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticUygulama
+{
+    class Hesap
+    {
+        static decimal vergi;
+        public static void VergiYaz()
+        {
+            string m;
+            Console.WriteLine("Brüt gelirinizi girer misiniz?");
+            m = Console.ReadLine();
+            Vergiler.brutGelir = decimal.Parse(m);
+            vergi = Vergiler.GelirVergisiHesapla(Vergiler.brutGelir);
+            Console.WriteLine("{0}'ın vergisi {1} TL'dir.",Vergiler.brutGelir,vergi);
+        }
+    }
+}
---
+++ StaticUygulama/Vergiler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticUygulama
+{
+    class Vergiler
+    {
+        public static decimal brutGelir;
+        const byte vergiYuzdesi=30;
+
+        public static decimal GelirVergisiHesapla(decimal x)
+        {
+            decimal y;
+            brutGelir = x;
+            y = (x * vergiYuzdesi) / 100;
+            return y;
+        }
+
+    }
+}
